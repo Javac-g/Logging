abe064ae456ca1a0800314508108a3166137ba55
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-05-24 19:26:55 +0300

    JUL workin fine

--- Main.java
+++ Main.java
@@ -15,14 +15,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("Logger.properties"));
-        LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.INFO);
-        logger.setLevel(Level.INFO);
+        LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("logging.properties"));
+//        LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.INFO);
+//        logger.setLevel(Level.INFO);
         logger.info(message);
         logger.info(messageSupplier);
         logger.log(Level.INFO,message);
         logger.logp(Level.INFO,"Class: Main","Method: main",message);
-        logger.logrb(Level.INFO,"Class: Main","Method: main",resourceBundle,"message id");
+        logger.logrb(Level.INFO,"Class: Main","Method: main",resourceBundle,"id");
 
     }
 }
